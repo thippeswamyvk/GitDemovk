7023f481b7d4d0375133f4c7991f5a163f860b26
thippeswamyvk <thippeshavk@gmail.com>
2025-01-26 01:38:00 +0530

    updated from Main person

--- src/test/java/rahulshettyacademy/tests/Erro2Val.java
+++ src/test/java/rahulshettyacademy/tests/Erro2Val.java
@@ -6,7 +6,21 @@ public class Erro2Val {
 		// TODO Auto-generated method stub
 		
 		System.out.println("Hi");
+		
+		int multi=subMethod();
+		System.out.println(multi);
+		
 
+	}
+	
+	public static int subMethod() {
+		int a=1;
+		int b=3;
+		int c=5;
+		int mul=a*b*c;
+		
+		return mul;
+		
 	}
 
 }
